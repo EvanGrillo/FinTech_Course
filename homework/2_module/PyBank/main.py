bf6a9d0677744cb2b5761ad278ad1a18f39a6cff
EvanGrillo <evan.grillo@outlook.com>
2023-10-21 20:35:59 -0500

    Try csv.DictReader with python script

--- homework/2_module/PyBank/main.py
+++ homework/2_module/PyBank/main.py
@@ -1,4 +1,7 @@
 import csv
+import os
+
+file_path = os.path.relpath('Homework/2_module/PyBank/Resources/budget_data.csv')
 
 total_months = 0
 total = 0
@@ -11,42 +14,44 @@ max_decrease = {
     'amount': 0
 }
 
-# To do - rework without these variables
-curr_val = None
 price_changes = []
+data_list = []
 
-with open('Resources/budget_data.csv') as f:
+with open(file_path) as f:
     
-    lines = f.readlines()
+    reader = csv.DictReader(f)
+
+    for row in reader:
+        data_list.append(row)
+
+    total_months = len(data_list)
+
+    for i, dict in enumerate(data_list):
+
+        total += int(dict["Profit/Losses"])
+
+        #skip first item, no previous to compare
+        if i == 0: continue
+            
+        prev = data_list[i-1]
+        
+        diff = int(dict["Profit/Losses"]) - int(prev["Profit/Losses"])
+                
+        price_changes.append(diff)
+
+        if diff > max_increase['amount']: 
+            max_increase['date'] = str(dict["Date"])
+            max_increase['amount'] = diff
+        if diff < max_decrease['amount']: 
+            max_decrease['date'] = str(dict["Date"])
+            max_decrease['amount'] = diff
     
-    # Skip header line in for loop
-    for line in lines[1:]:
-        
-        # Use String split to parse row values with comma delimiter
-        val1,val2 = line.split(',', 1)
-        
-        total_months += 1
-        total += int(val2)
-        
-        # Use pointer to store current value to avoid extra steps
-        if curr_val != None:
-            
-            diff = int(val2) - curr_val
-            price_changes.append(diff)
-            
-            if diff > max_increase['amount']: 
-                max_increase['date'] = str(val1)
-                max_increase['amount'] = diff
-            if diff < max_decrease['amount']: 
-                max_decrease['date'] = str(val1)
-                max_decrease['amount'] = diff
-
-        curr_val = int(val2)
-
 print(
-    f'Total Months: {total_months}\n'
-    f'Total: ${total}\n'
-    f'Average Change: ${round(sum(price_changes) / len(price_changes), 2)}\n'
-    f'Greatest Increase in Profits: {max_increase["date"]} ${max_increase["amount"]}\n'
-    f'Greatest Decrease in Profits: {max_decrease["date"]} ${max_decrease["amount"]}\n'
+f"""
+Total Months: {total_months}
+Total: ${total}
+Average Change: ${round(sum(price_changes) / len(price_changes), 2)}
+Greatest Increase in Profits: {max_increase["date"]} ${max_increase["amount"]}
+Greatest Decrease in Profits: {max_decrease["date"]} ${max_decrease["amount"]}
+"""
 )
