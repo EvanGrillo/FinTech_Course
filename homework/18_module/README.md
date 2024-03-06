2f68f1eb64b8a20868e1fdd17f30bc68e54e738d
EvanGrillo <evan.grillo@outlook.com>
2024-03-05 20:40:01 -0600

    Add files for module 18 hw

---
+++ homework/18_module/README.md
@@ -0,0 +1,11 @@
+# PyChain Ledger with Streamlit
+
+## Run Streamlit App
+```shell
+streamlit run pychain.py
+```
+
+## Execute Bash Script
+```shell
+sh proof_of_work.sh #optional args include a file path and a block difficulty
+```
---
+++ homework/18_module/proof_of_work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Generate fallback file
+if [ -z "$1"]
+then
+    echo "test" > test.txt
+fi
+
+filename=${1:-"test.txt"}
+target_prefix=${2:-"00"}
+hash_prefix=""
+attempts=0
+
+while [ "$hash_prefix" != "$target_prefix" ]; do
+
+    # Generate timestamp, pipe to head and truncate first 32 chars
+    echo "$(date +%s%N | head -c 32)" >> "$filename"
+    
+    # Calculate the SHA-256 hash of the file
+    hash_prefix=$(shasum -a 256 "$filename" | cut -c 1-2)
+
+    ((attempts++))
+    
+    echo "Attempt $attempts - Current hash prefix: $hash_prefix"
+    
+done
+
+echo "Hash prefix is now: $hash_prefix"
+echo "File content:"
+cat "$filename"
+echo "Number of attempts: $attempts"
+
